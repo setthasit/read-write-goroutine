d42db9c4f782082e6cb7c277c24afd450ce55616
Art Thungkawee <art.t@oozou.com>
2021-10-22 20:17:40 +0700

    add test  for read digit equality

---
+++ function/readfile_test.go
@@ -0,0 +1,32 @@
+package function
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadDigit(t *testing.T) {
+	filePath := "TestReadDigit.txt"
+	testText := "987654321"
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	file.Close()
+	defer os.Remove(filePath)
+
+	err = ioutil.WriteFile(filePath, []byte(testText), 0222)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	file, scanner := ReadDigitsFromFile(filePath)
+	defer file.Close()
+
+	scanner.Scan()
+	assert.Equal(t, testText, scanner.Text())
+}
