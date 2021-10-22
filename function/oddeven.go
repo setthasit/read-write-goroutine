d2ba9ff379de190d29f8ec8bea0c76fa4f5bfc6c
Art Thungkawee <art.t@oozou.com>
2021-10-22 18:15:55 +0700

    change output to use log due to unit test

--- function/oddeven.go
+++ function/oddeven.go
@@ -1,19 +1,21 @@
 package function
 
-import "fmt"
+import (
+	"log"
+)
 
 // printEvenOdd - Check and print if the number is even or odd
 func printEvenOdd(n *int) {
 	// Check if text is nil
 	if n == nil {
-		print("cannot print even/odd: argument is nil")
+		log.Print("cannot print even/odd: argument is nil")
 		return
 	}
 
 	// Check if the number is even or odd and print the number
 	if *n == 0 || *n%2 == 0 {
-		fmt.Printf("%d is is an even number\n", *n)
+		log.Printf("%d is is an even number\n", *n)
 		return
 	}
-	fmt.Printf("%d is is an odd number\n", *n)
+	log.Printf("%d is is an odd number\n", *n)
 }
