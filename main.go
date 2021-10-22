de93518d7015f0d0d0b1093d1d95ac657f34972a
Art Thungkawee <art.t@oozou.com>
2021-10-22 15:24:06 +0700

    add init function to the file

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func init() {
+	// declare the seed value in order to make the random number chage overtime
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	// Check if the file exist, if not will create the new file
+	if _, err := os.Stat("digits.txt"); err != nil {
+		file, err := os.Create("digits.txt")
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		file.Close()
+		return
+	}
+	// If file exist, clear the content in the file
+	err := os.Truncate("digits.txt", 0)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
 func main() {
 	println("Bello")
 }
