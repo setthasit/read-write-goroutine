7b5498030a55a615e1fe03aee575dd43d9f4bef8
Art Thungkawee <art.t@oozou.com>
2021-10-22 19:50:19 +0700

    added test for validate the lenght of the file content

---
+++ function/writefile_test.go
@@ -0,0 +1,51 @@
+package function
+
+import (
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func checkfile(filePath string) {
+	// remove prefix & flag form log output
+	log.SetFlags(0)
+	// Declare the seed value in order to make the random number chage overtime
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	// Check if the file exist, if not will create the new file
+	if _, err := os.Stat(filePath); err != nil {
+		file, err := os.Create(filePath)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		file.Close()
+		return
+	}
+	// If file exist, clear the content in the file
+	err := os.Truncate(filePath, 0)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func TestWriteLenght(t *testing.T) {
+	filePath := "TestWriteLenght.txt"
+
+	checkfile(filePath)
+	defer os.Remove(filePath)
+
+	WriteRandomDigitsToFile(filePath, 10)
+
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
+	}
+
+	assert.Equal(t, 10, len(string(file)))
+}
