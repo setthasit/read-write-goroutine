eed01bad3767796982e41262b791042f273bf25f
Art Thungkawee <art.t@oozou.com>
2021-10-23 14:11:52 +0700

    receive digit length as command argument

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"os"
 	"read-digit/function"
 	"runtime"
+	"strconv"
 	"sync"
 	"time"
 )
 
 var filePath = "digits.txt"
+var digitsLength int64
 
 func init() {
 	// Check if CPU >= 2 cores
@@ -18,13 +20,14 @@ func init() {
 		runtime.GOMAXPROCS(2)
 	}
 
-	// remove prefix & flag form log output
+	// Remove prefix & flag form log output
 	log.SetFlags(0)
 
 	// Declare the seed value in order to make the random number chage overtime
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	// Check if the file exist, if not will create the new file
+	// If file already exist, will clear the content in the file
 	if _, err := os.Stat(filePath); err != nil {
 		file, err := os.Create(filePath)
 		if err != nil {
@@ -32,11 +35,25 @@ func init() {
 		}
 		file.Close()
 		return
+	} else {
+		err := os.Truncate(filePath, 0)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
 	}
-	// If file exist, clear the content in the file
-	err := os.Truncate(filePath, 0)
-	if err != nil {
-		log.Fatal(err.Error())
+
+	// Check if there command argument
+	// argument should be number
+	digitsLength = 2048
+	if os.Args[1] != "" {
+		argLength, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		if argLength > 0 {
+			digitsLength = int64(argLength)
+		}
 	}
 }
 
@@ -44,11 +61,12 @@ func main() {
 	// Mutex use for preventing race condition
 	var mu sync.RWMutex
 	// Channel that contain position of new digits
-	changes := make(chan int64, 2048)
+	changes := make(chan int64, digitsLength)
 
 	// Write the digits to file concurently
-	go function.WriteRandomDigitsToFile(filePath, 2048, changes, &mu)
+	go function.WriteRandomDigitsToFile(filePath, digitsLength, changes, &mu)
 
+	// Read the digits and print to console
 	for change := range changes {
 		n := function.ReadDigitsFromFilePosition(filePath, change, &mu)
 		function.PrintEvenOdd(&n)
