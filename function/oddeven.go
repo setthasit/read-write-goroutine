81348de14a46cc341793afaa9c6c7b93639180c4
Art Thungkawee <art.t@oozou.com>
2021-10-23 15:01:10 +0700

    fix typo when print the even/odd

--- function/oddeven.go
+++ function/oddeven.go
@@ -14,8 +14,8 @@ func PrintEvenOdd(n *int) {
 
 	// Check if the number is even or odd and print the number
 	if *n == 0 || *n%2 == 0 {
-		log.Printf("%d is is an even number\n", *n)
+		log.Printf("%d is an even number\n", *n)
 		return
 	}
-	log.Printf("%d is is an odd number\n", *n)
+	log.Printf("%d is an odd number\n", *n)
 }
